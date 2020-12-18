6452ede50121c506d1c067db1007e66c0f8abdfa
Simon Roberts <simon@dancingcloudservices.com>
2020-12-18 09:11:45 -0700

    Break One

--- hello.py
+++ hello.py
@@ -1,1 +1,43 @@
-print("Hello Python World!")
+print('Hello "Python" World!', 99, sep="\n")
+x=101
+print("the value is", x)
+# Concatenate Strings--yes :)
+name="Fred" + " Jones"
+print(name)
+# Don't have autoconversion to string with concatenation
+# name=7 + "of nine"
+# print(name)
+name=str(7) + " of nine"
+print(name)
+
+x = 10
+y = "11"
+z = x + int(y)
+print(z)
+
+print(" \"hello\" she said ")
+
+x = 10.2
+y = 3
+print(type(x))
+print(type(y))
+
+print(3/2)
+print(3//2)
+print(3.5//2)
+
+print(2**16)
+
+x = 10
+x += 3 # equivalent to x = x + 3
+print(x)
+
+# walrus new with 3.8
+print(x := 99)
+print(x)
+
+x = 9E+300
+print(x)
+
+x = int(3E200)
+print(x)
